320f95cd5e63824d2e02d8633c5976d1027b3071
w1z13 <115799235+w1z13@users.noreply.github.com>
2023-04-17 15:58:57 +0200

    removed console.log

--- scripts.js
+++ scripts.js
@@ -1,10 +1,8 @@
 function knightMoves(from, dest) {
     const moves = [[1, 2], [-1, 2], [-1, -2], [1, -2], [2, 1], [2, -1], [-2, -1], [-2, 1]];
     const queue = [from];
-    //console.log(queue);
 
     for (let i = 0; i < queue.length; i++){
-        console.log(queue[i], [i]);
         if (queue[i][0] === dest[0] && queue[i][1] === dest[1]) return display(queue[i]);   
         for (let j = 0; j < moves.length; j++) {
             const next = [queue[i][0] + moves[j][0], queue[i][1] + moves[j][1]];
